f696b2c04b8794c159e118d35019247f68eb0672
udobes <github@besenreuther.de>
2022-03-24 19:15:18 +0100

    Änderungen von heute

--- P05_ESP32now_Fahrradkommunikation/09_ESP32now_Beep_PlattformIO/src/main.cpp
+++ P05_ESP32now_Fahrradkommunikation/09_ESP32now_Beep_PlattformIO/src/main.cpp
@@ -9,12 +9,6 @@
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files.
 
-
-
-leerzeile
-
-
-
   The above copyright notice and this permission notice shall be included in all
   copies or substantial portions of the Software.
 */
